2d2873169e95135be7444a3f6aaada43ec96f687
ahesss <mangkok.pepes01@gmail.com>
2026-03-09 22:24:34 +0700

    Update maxPrice to 0.335

--- bot.py
+++ bot.py
@@ -46,21 +46,21 @@ COUNTRIES = {
         "flag": "🇻🇳",
         "country_id": "10",
         "country_code": "84",
-        "maxPrice": "0.2",
+        "maxPrice": "0.335",
     },
     "colombia": {
         "name": "Colombia",
         "flag": "🇨🇴",
         "country_id": "33",
         "country_code": "57",
-        "maxPrice": "0.2",
+        "maxPrice": "0.335",
     },
     "philipina": {
         "name": "Philipina",
         "flag": "🇵🇭",
         "country_id": "4",
         "country_code": "63",
-        "maxPrice": "0.2",
+        "maxPrice": "0.335",
     },
 }
 
