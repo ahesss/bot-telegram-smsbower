5ba6aab732d34a453e235a85501ef5bd78202841
ahesss <mangkok.pepes01@gmail.com>
2026-03-10 00:01:54 +0700

    Update bot.py (add autobuy and overlay)

--- bot.py
+++ bot.py
@@ -46,21 +46,21 @@ COUNTRIES = {
         "flag": "🇻🇳",
         "country_id": "10",
         "country_code": "84",
-        "maxPrice": "0.20",
+        "maxPrice": "0.25",
     },
     "philipina": {
         "name": "Philipina",
         "flag": "🇵🇭",
         "country_id": "4",
         "country_code": "63",
-        "maxPrice": "0.20",
+        "maxPrice": "0.25",
     },
     "colombia": {
         "name": "Colombia",
         "flag": "🇨🇴",
         "country_id": "33",
         "country_code": "57",
-        "maxPrice": "0.20",
+        "maxPrice": "0.25",
     },
 }
 
